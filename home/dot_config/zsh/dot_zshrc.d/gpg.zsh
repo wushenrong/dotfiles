da21b6d640e81186eac5db3d6442d868a2d9c489
Samuel Wu <twopizza9621536@gmail.com>
2024-11-25 08:51:28 -0500

    Move configuration from dotfiles to zshrc (#2)
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/gpg.zsh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2024 Samuel Wu
-#
-# SPDX-License-Identifier: 0BSD
-
-if [[ -n "$TTY" ]]; then
-  export GPG_TTY=$TTY
-else
-  export GPG_TTY=$(tty)
-fi
--- home/dot_config/zsh/dot_zshrc.d/zoxide.zsh
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2024 Samuel Wu
-#
-# SPDX-License-Identifier: 0BSD
-
-(( $+commands[zoxide] )) || return 1
-source <(zoxide init zsh --cmd cd)
