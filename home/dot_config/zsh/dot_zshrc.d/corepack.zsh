6efe370d7676c63b676037e9f82c12631c65bf70
Samuel Wu <twopizza9621536@gmail.com>
2024-11-25 08:51:28 -0500

    Move configuration from dotfiles to zshrc (#2)
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/corepack.zsh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2024 Samuel Wu
-#
-# SPDX-License-Identifier: 0BSD
-
-(( $+commands[corepack] )) || return 1
-alias yarn='corepack yarn'
-alias yarnpkg='corepack yarnpkg'
-alias pnpm='corepack pnpm'
-alias pnpx='corepack pnpx'
