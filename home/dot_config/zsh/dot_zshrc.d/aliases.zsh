447d7f9a5c35c5014fe26f3c1184c4f558b706e4
Samuel Wu <twopizza9621536@gmail.com>
2024-12-16 08:26:32 -0500

    Simpify the loading of zsh plugins
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/aliases.zsh
+++ home/dot_config/zsh/dot_zshrc.d/aliases.zsh
@@ -27,7 +27,6 @@ alias print-path='echo $PATH | tr ":" "\n"'
 alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
 
 # misc
-alias load_antidote='source $ZDOTDIR/.antidote/antidote.zsh'
 alias zshrc='$EDITOR $ZDOTDIR/.zshrc'
 alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
 alias zdot='cd $ZDOTDIR'
