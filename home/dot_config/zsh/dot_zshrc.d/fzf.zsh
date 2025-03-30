968ad2193e3e24f5405f1ae8a747486660664f04
Samuel Wu <twopizza9621536@gmail.com>
2025-03-30 09:09:56 -0400

    Add package install scripts
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/fzf.zsh
+++ home/dot_config/zsh/dot_zshrc.d/fzf.zsh
@@ -12,24 +12,14 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --color=always"
 export FZF_ALT_C_COMMAND="fd -t d"
 
 export FZF_DEFAULT_OPTS="--preview-window=right:60%,wrap"
-export FZF_CTRL_T_OPTS="--ansi"
-
-if (( $+commands[eza] )); then
-  export FZF_ALT_C_OPTS="--preview 'eza -1F -sName --group-directories-first --icons --color=always {}'"
-else
-  export FZF_ALT_C_OPTS="--preview 'ls -1F --group-directories-first --color=always {}'"
-fi
-
-if (( $+commands[bat] )); then
-  export FZF_CTRL_T_OPTS="$FZF_CTRL_T_OPTS --preview 'bat -n --color=always {}'"
-elif (($ + commands[batcat])); then
-  export FZF_CTRL_T_OPTS="$FZF_CTRL_T_OPTS --preview 'batcat -n --color=always {}'"
-fi
+export FZF_CTRL_T_OPTS="--ansi --preview '~/.lessfilter {} || less {}'"
+export FZF_ALT_C_OPTS="--preview 'eza -1F -sName --group-directories-first --icons --color=always {}'"
 
 if command fzf --zsh 2>/dev/null; then
   source <(fzf --zsh)
 else
   source /usr/share/doc/fzf/examples/key-bindings.zsh
+  source /usr/share/doc/fzf/examples/completion.zsh
 fi
 
 # Enable fzf-tab
