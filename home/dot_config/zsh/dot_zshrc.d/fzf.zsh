af7631ce4316a981fd7dfe766e1ebc073f0dedbe
Samuel Wu <twopizza9621536@gmail.com>
2025-04-11 13:19:59 -0400

    Update FZF Opts
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/fzf.zsh
+++ home/dot_config/zsh/dot_zshrc.d/fzf.zsh
@@ -6,8 +6,8 @@ export FZF_DEFAULT_COMMAND="bfs -type f -mindepth 1 -L -exclude -name .git -prin
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND -color"
 export FZF_ALT_C_COMMAND="bfs -type d -mindepth 1 -nohidden -printf '%P\n'"
 
-export FZF_CTRL_T_OPTS="--ansi --preview '~/.lessfilter {} || less {}'"
-export FZF_ALT_C_OPTS="--preview 'eza -1F -sName --group-directories-first --icons --color=always {}'"
+export FZF_CTRL_T_OPTS="--select-1 --exit-0 --ansi --preview '~/.lessfilter {} || less {}'"
+export FZF_ALT_C_OPTS="--select-1 --exit-0 --preview 'eza -1F -sName --group-directories-first --icons --color=always {}'"
 
 if command fzf --zsh >/dev/null 2>&1; then
   source <(fzf --zsh)
