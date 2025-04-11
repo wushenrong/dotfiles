0abf623dea244852d4518928402c1a686c88f19f
Samuel Wu <twopizza9621536@gmail.com>
2025-04-10 21:09:29 -0400

    Use native binary for cmake-format on debain
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- scripts/linux/debian/20-python.sh
+++ scripts/linux/debian/20-python.sh
@@ -3,11 +3,10 @@
 #
 # SPDX-License-Identifier: 0BSD
 
-sudo apt install -y pipx
+sudo apt install -y pipx cmake-format
 
 pipx install uv
 
 uv tool install pre-commit
 uv tool install reuse
 uv tool install cpplint
-uv tool install cmakelang[YAML]
