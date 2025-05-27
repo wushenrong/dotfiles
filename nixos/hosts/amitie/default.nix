5ef54937a493466b32650dc61c17aaf08fea224e
Samuel Wu <twopizza9621536@gmail.com>
2025-05-27 15:50:42 -0400

    feat(nixos): 🔒 Add back newly hashed password
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- nixos/hosts/amitie/default.nix
+++ nixos/hosts/amitie/default.nix
@@ -6,7 +6,7 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = ["wheel" "networkmanager" "audio"];
-    hashedPassword = "INSERT_HASHED_PASSWORD_HERE";
+    hashedPassword = "$y$j9T$/DAp0/.UdGPSVEP2v2hz.1$vH7lK.0eVYB3lPCFITwfh4Oz.4fflq9b4nXjkKhz7R6";
   };
 
   services.openssh = {
