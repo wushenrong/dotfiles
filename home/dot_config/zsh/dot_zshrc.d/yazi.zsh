429afdc0205f298158d8fd9b19b2d28a9870a09f
Samuel Wu <twopizza9621536@gmail.com>
2025-05-30 23:34:00 -0400

    feat: ✨ Add dependencies for yazi
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ home/dot_config/zsh/dot_zshrc.d/yazi.zsh
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 Samuel Wu
+#
+# SPDX-License-Identifier: 0BSD
+
+(( $+commands[yazi] )) || return 1
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
