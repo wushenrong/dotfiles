485b9a5af2a379e3e3df37de71891e5782e33d58
Samuel Wu <twopizza9621536@gmail.com>
2025-06-25 12:14:33 -0400

    feat(zsh): ✨ Update aliases
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- home/dot_config/zsh/dot_zshrc.d/lesspipe.zsh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Samuel Wu
-#
-# SPDX-License-Identifier: 0BSD
-
-if (( $+commands[lesspipe.sh] )); then
-  source <(lesspipe.sh)
-elif (( $+commands[lesspipe] )); then
-  source <(lesspipe)
-fi
